998f72ed9421974a39a4f2017e7a57454ef57642
caoyue <caoyue@wdai.com>
2019-03-19 17:02:25 +0800

    uml

--- src/com/crystal/pattern/factory/simple/Factory.java
+++ src/com/crystal/pattern/factory/simple/Factory.java
@@ -3,5 +3,5 @@ package com.crystal.pattern.factory.simple;
 import com.crystal.pattern.factory.Fridge;
 
 public interface Factory {
-    public Fridge produceFridge(String name);
+    Fridge produceFridge(String name);
 }
